ed883a1e2220f5753d9bb7e17cd911f57d7006c8
smehrabi <35901230+smehrabi@users.noreply.github.co>
2020-05-26 23:43:47 -0700

    Finished the tutorial with dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,19 +1,36 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
 
 export default function Post({ postData }) {
     return (
       <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
+        <Head>
+          <title>
+            {postData.title}
+            
+            </title>
+
+        </Head>
+        <article>
+        <h1 className={utilStyles.headingXL}>
+          {postData.id}
+          
+          </h1>
+          <div className={utilStyles.lightText}>
+        <Date dateString={postData.date} />
+        </div>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+
       </Layout>
     )
   }
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
       props: {
         postData
